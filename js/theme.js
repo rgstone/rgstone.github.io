54e7e9b2dcc8528dc8291b89bd10c404aaacc35a
cywu <chywu@fudan.edu.cn>
2017-03-15 20:57:01 +0800

    cleaned up theme setup code

---
+++ js/theme.js
@@ -0,0 +1,76 @@
+//Given today's date return theme CSS file path
+var themeSetter=function (today) {
+    var dirPrefix = Array(depthOfCurrentFile ()).join("../"); //to get to the `root' dir
+    var thisYear = today.getFullYear();
+    var holidayList = {
+	"newYear1": {"name": "new-year",
+		     "date": new Date(thisYear, 0, 1),
+		     "fore": 0,
+		     "aft": 3},
+	"newYear2": {"name": "new-year",
+		     "date": new Date(thisYear + 1, 0, 1),
+		     "fore": 3,
+		     "aft": 0},
+	//ad hoc patch for chinese new year. 
+	"springfestival":{"name": "spring-festival",
+			  "date": new Date(2018, 1, 16),
+			  "fore": 0,
+			  "aft": 6},
+	"valentine": {"name": "214",
+		      "date": new Date(thisYear, 1, 14),
+		      "fore": 0,
+		      "aft": 1},
+	"apr1": {"name": "apr1",
+		 "date": new Date(thisYear, 3, 1),
+		 "fore": 0,
+		 "aft": 1},
+	"halloween": {"name": "halloween",
+		      "date": new Date(thisYear, 9, 31),
+		      "fore": 15,
+		      "aft": 1},
+	"thanksgiving": {"name": "thanksgiving",
+			 "date": getDateNthDay(new Date(thisYear,10,1),4,4),
+			 "fore": 15,
+			 "aft": 4},
+	"xmas": {"name": "xmas",
+		 "date": new Date(thisYear, 11, 25),
+		 "fore": 15,
+		 "aft": 4}
+    };
+    
+    var themeCSSPath = function() {
+	var str=this.theme ();
+	if (str===null) {
+	    str="1";
+	}
+	return dirPrefix + "css/style-" + str + ".css";
+    };
+
+    var theme = function () {
+	var src = dirPrefix + "css/style1.css";
+	for (h in holidayList) {
+	    var start = new Date(holidayList[h] ["date"]);
+	    start.setDate(start.getDate() - holidayList[h]["fore"]);
+	    var end  = new Date(holidayList[h] ["date"]);
+	    end.setDate(end.getDate() + holidayList[h] ["aft"]);
+	    if (today >= start && today < end) {
+		return holidayList[h] ["name"];
+	    }
+	}
+	return null;
+    };
+    return {theme:theme,
+	    themeCSSPath:themeCSSPath};
+};
+
+
+
+
+
+
+var today = new Date(2017,3,1);
+
+
+
+
+
--- js/util.js
+++ js/util.js
@@ -10,3 +10,42 @@ function week2Date(startDate,i) {
     return date2String(date1,"/")+ " -- " + date2String(date2,"/");
 
 }
+
+function date2String(dd,z) {
+    z = z|| ""; 
+    return pad(dd.getFullYear(),4,'0') + z + pad((dd.getMonth()+1),2,'0') + z + pad(dd.getDate(),2,'0'); 
+}
+
+function getDateNthDay(date, i, d) {
+    // get date of i-th d-day of the month specified in date
+    var date1 = new Date(date);
+    date1.setDate(1); //first day of the month
+    var dayDate1 = date1.getDay();
+    //find i-th d-day
+    var dd = d - dayDate1 + 1 + i * 7;
+    if (dayDate1 <= d) {
+	dd = dd - 7;
+    }
+    //compute the total number of days in the given month
+    var lastDate = new Date(date1);
+    lastDate.setMonth(lastDate.getMonth() + 1, 0);
+    //test if dd is out of range
+    if (dd < 1 || dd > lastDate.getDate()) {
+	return null;
+    } else {
+	date1.setDate(dd)
+	return date1;
+    }
+}
+
+function depthOfCurrentFile(){
+    (window.location.pathname.match(/\//g) || []).length;
+}
+
+    function pad(n, width, z) {
+    //pads a number to specified width with given character z.
+    //number not trimmed if length larger than width.
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
